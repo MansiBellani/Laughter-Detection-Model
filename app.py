a416fc8a87d87f553db4da2f5eafab1257221d83
MansiBellani <90083434+MansiBellani@users.noreply.github.com>
2023-03-09 10:27:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from prediction import predict
-st.title("Laughter Detection")
-st.markdown("Toy model to play to classify iris flowers into \
+# st.title("Laughter Detection")
+# st.markdown("Toy model to play to classify iris flowers into \
 setosa, versicolor, virginica")
 
 st.button(“Predict type of Iris”)
