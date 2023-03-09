d0217507658ed7597ff1506f0190b1d318bee881
MansiBellani <90083434+MansiBellani@users.noreply.github.com>
2023-03-09 10:26:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from prediction import predict
-st.title(‘Laughter Detection’)
+# st.title(Laughter Detection")
 st.markdown(‘Toy model to play to classify iris flowers into \
 setosa, versicolor, virginica’)
 
